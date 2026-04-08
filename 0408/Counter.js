af4145c8a2dc2952c1afcd5be12b4fc45e23d799
dlguddnjs0111-ops <dlguddnjs0111@gmail.com>
2026-04-08 10:39:34 +0900

    Update Counter.js

--- 0408/Counter.js
+++ 0408/Counter.js
@@ -18,3 +18,26 @@ const Counter = () => {
     )
 }
 export default Counter;
+
+
+#state를 사용하는 경우
+
+import { useState } from 'react';
+const Counter = () => {
+    const [count, setCount] = useState(0);
+    console.log(count);
+    const plus = () => {
+        setCount(count + 1);
+    }
+    const minus = () => {
+        setCount(count - 1);
+    }
+    return (
+        <div className='container'>
+            <h2 className='int'>{count}</h2>
+            <button className='plus' onClick={plus}>+</button>
+            <button className='minus' onClick={minus}>-</button>
+        </div>
+    )
+}
+export default Counter;
